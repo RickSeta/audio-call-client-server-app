2856d3e90ae063e77f17614ec339179486d271e7
Gabriel Brandão <gafbrandao@id.uff.br>
2022-11-08 12:50:11 -0300

    audio recording

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import pyaudio
 
+#def record():
+# Default is Rate = 44100 Hz and Format = 24 bits
+FORMAT=pyaudio.paInt24
+RATE=44100
+CHANNELS=1
+CHUNK=1024
+RECORD_SECONDS=5
+
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+
+print("* Recording Started")
+
+for i in range(0, int(RATE/CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+
+print("* Recording Finished")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
