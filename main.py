74a607f67adf5926caa06c468ac1499bdef56604
Gabriel Brandão <gabriel.brandao@hashdex.com>
2022-11-21 19:22:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ RECORD_SECONDS=5
 
 p = pyaudio.PyAudio()
 stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+frames = []
 
 print("* Recording Started")
 
 for i in range(0, int(RATE/CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
+    frames.append(data)
 
 print("* Recording Finished")
 
